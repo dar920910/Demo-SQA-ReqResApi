2157e659e69a848801951bf0d4cd1fdb128ed8a6
Denis Rezepov <me@dar920910.ru>
2023-05-04 17:51:13 +0300

    Connected pytest framework to the project and added tiny tests.

--- runner.ps1
+++ runner.ps1
@@ -1,1 +1,2 @@
 .\.venv\Scripts\python.exe .\src\main.py
+.\.venv\Scripts\python.exe -m pytest -v .\src\test_demo.py
---
+++ src/test_demo.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_case_01():
+    assert 'REQRES'.upper() == 'REQRES'
+
+def test_case_02():
+    assert 'reqres'.lower() == 'reqres'
