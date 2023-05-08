f0849bbc8b7bf27680503fb055e3dfe2f735fd08
Denis Rezepov <me@dar920910.ru>
2023-05-08 21:59:39 +0300

    Prepared other API tests (POST-requests and etc).

--- src/test_suite_reqres_api.py
+++ src/test_suite_reqres_api.py
@@ -111,37 +111,165 @@ def test_get_no_existing_single_resource_by_id_to_check_response_body():
     assert actual_response_body == expected_response_body
 
 
-# positive
-def create_new_user():
-    pass
+# REQUEST_07: CREATE
 
-# positive
-def put_update_user():
-    pass
+api_request_07_body = json.loads(api_reqres_v0.API_V0_CREATE_REQUEST_BODY)
+api_response_07 = requests.post("https://reqres.in" + api_reqres_v0.API_V0_CREATE_REQUEST_URL, api_request_07_body)
 
-# positive
-def patch_update_user():
-    pass
+def test_create_new_user_to_check_response_code():
+    """test_create_new_user_to_check_response_code"""
+    actual_response_code = api_response_07.status_code
+    expected_response_code = api_reqres_v0.API_V0_CREATE_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
 
-# positive
-def delete_user():
-    pass
+actual_response_07_body = api_response_07.json()
 
-# positive
-def successful_register():
-    pass
+def test_new_user_id():
+    """test_new_user_id"""
+    assert int(actual_response_07_body['id']) > 0
 
-# negative
-def unsuccessful_register():
-    pass
+def test_new_user_name_attribute():
+    """test_new_user_name_attribute"""
+    assert actual_response_07_body['name'] == api_request_07_body['name']
 
-# positive
-def successful_login():
-    pass
+def test_new_user_job_attribute():
+    """test_new_user_job_attribute"""
+    assert actual_response_07_body['job'] == api_request_07_body['job']
 
-# negative
-def unsuccessful_login():
-    pass
+def test_new_user_creation_attribute():
+    """test_new_user_creation_attribute"""
+    assert actual_response_07_body['createdAt'] != ""
+
+
+# REQUEST_08: UPDATE
+
+api_request_08_body = json.loads(api_reqres_v0.API_V0_UPDATE_PUT_REQUEST_BODY)
+api_response_08 = requests.put("https://reqres.in" + api_reqres_v0.API_V0_UPDATE_PUT_REQUEST_URL, api_request_08_body)
+
+def test_put_update_user_to_check_response_code():
+    """test_put_update_user_to_check_response_code"""
+    actual_response_code = api_response_08.status_code
+    expected_response_code = api_reqres_v0.API_V0_UPDATE_PUT_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+actual_response_08_body = api_response_08.json()
+
+def test_put_update_user_name_attribute():
+    """test_put_update_user_name_attribute"""
+    assert actual_response_08_body['name'] == api_request_08_body['name']
+
+def test_put_update_user_job_attribute():
+    """test_put_update_user_job_attribute"""
+    assert actual_response_08_body['job'] == api_request_08_body['job']
+
+def test_put_update_user_updateat_attribute():
+    """test_put_update_user_updateat_attribute"""
+    assert actual_response_08_body['updatedAt'] != ""
+
+
+# REQUEST_09: UPDATE
+
+api_request_09_body = json.loads(api_reqres_v0.API_V0_UPDATE_PATCH_REQUEST_BODY)
+api_response_09 = requests.patch("https://reqres.in" + api_reqres_v0.API_V0_UPDATE_PATCH_REQUEST_URL, api_request_09_body)
+
+def test_patch_update_user_to_check_response_code():
+    """test_patch_update_user_to_check_response_code"""
+    actual_response_code = api_response_09.status_code
+    expected_response_code = api_reqres_v0.API_V0_UPDATE_PATCH_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+actual_response_09_body = api_response_09.json()
+
+def test_patch_update_user_name_attribute():
+    """test_patch_update_user_name_attribute"""
+    assert actual_response_09_body['name'] == api_request_09_body['name']
+
+def test_patch_update_user_job_attribute():
+    """test_patch_update_user_job_attribute"""
+    assert actual_response_09_body['job'] == api_request_09_body['job']
+
+def test_patch_update_user_updateat_attribute():
+    """test_patch_update_user_updateat_attribute"""
+    assert actual_response_09_body['updatedAt'] != ""
+
+
+# REQUEST_10: DELETE
+
+api_response_10 = requests.delete("https://reqres.in" + api_reqres_v0.API_V0_DELETE_REQUEST_URL)
+
+def test_delete_user_by_id_to_check_response_code():
+    """test_delete_user_by_id_to_check_response_code"""
+    actual_response_code = api_response_10.status_code
+    expected_response_code = api_reqres_v0.API_V0_DELETE_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+
+# REQUEST_11: REGISTER SUCCESSFUL
+
+api_response_11 = requests.post("https://reqres.in" + api_reqres_v0.API_V0_REGISTER_SUCCESSFUL_REQUEST_URL, json.loads(api_reqres_v0.API_V0_REGISTER_SUCCESSFUL_REQUEST_BODY))
+
+def test_successful_register_to_check_response_code():
+    """test_successful_register_to_check_response_code"""
+    actual_response_code = api_response_11.status_code
+    expected_response_code = api_reqres_v0.API_V0_REGISTER_SUCCESSFUL_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+def test_successful_register_to_check_response_body():
+    """test_successful_register_to_check_response_body"""
+    actual_response_body = api_response_11.json()
+    expected_response_body = json.loads(api_reqres_v0.API_V0_REGISTER_SUCCESSFUL_RESPONSE_BODY)
+    assert actual_response_body == expected_response_body
+
+
+# REQUEST_12: REGISTER UNSUCCESSFUL
+
+api_response_12 = requests.post("https://reqres.in" + api_reqres_v0.API_V0_REGISTER_UNSUCCESSFUL_REQUEST_URL, json.loads(api_reqres_v0.API_V0_REGISTER_UNSUCCESSFUL_REQUEST_BODY))
+
+def test_unsuccessful_register_to_check_response_code():
+    """test_unsuccessful_register_to_check_response_code"""
+    actual_response_code = api_response_12.status_code
+    expected_response_code = api_reqres_v0.API_V0_REGISTER_UNSUCCESSFUL_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+def test_unsuccessful_register_to_check_response_body():
+    """test_unsuccessful_register_to_check_response_body"""
+    actual_response_body = api_response_12.json()
+    expected_response_body = json.loads(api_reqres_v0.API_V0_REGISTER_UNSUCCESSFUL_RESPONSE_BODY)
+    assert actual_response_body == expected_response_body
+
+
+# REQUEST_13: LOGIN SUCCESSFUL
+
+api_response_13 = requests.post("https://reqres.in" + api_reqres_v0.API_V0_LOGIN_SUCCESSFUL_REQUEST_URL, json.loads(api_reqres_v0.API_V0_LOGIN_SUCCESSFUL_REQUEST_BODY))
+
+def test_successful_login_to_check_response_code():
+    """test_successful_login_to_check_response_code"""
+    actual_response_code = api_response_13.status_code
+    expected_response_code = api_reqres_v0.API_V0_LOGIN_SUCCESSFUL_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+def test_successful_login_to_check_response_body():
+    """test_successful_login_to_check_response_body"""
+    actual_response_body = api_response_13.json()
+    expected_response_body = json.loads(api_reqres_v0.API_V0_LOGIN_SUCCESSFUL_RESPONSE_BODY)
+    assert actual_response_body == expected_response_body
+
+
+# REQUEST_14: LOGIN UNSUCCESSFUL
+
+api_response_14 = requests.post("https://reqres.in" + api_reqres_v0.API_V0_LOGIN_UNSUCCESSFUL_REQUEST_URL, json.loads(api_reqres_v0.API_V0_LOGIN_UNSUCCESSFUL_REQUEST_BODY))
+
+def test_unsuccessful_login_to_check_response_code():
+    """test_unsuccessful_login_to_check_response_code"""
+    actual_response_code = api_response_14.status_code
+    expected_response_code = api_reqres_v0.API_V0_LOGIN_UNSUCCESSFUL_RESPONSE_CODE
+    assert actual_response_code == expected_response_code
+
+def test_unsuccessful_login_to_check_response_body():
+    """test_unsuccessful_login_to_check_response_body"""
+    actual_response_body = api_response_14.json()
+    expected_response_body = json.loads(api_reqres_v0.API_V0_LOGIN_UNSUCCESSFUL_RESPONSE_BODY)
+    assert actual_response_body == expected_response_body
 
 
 # REQUEST_15: DELAYED RESPONSE
