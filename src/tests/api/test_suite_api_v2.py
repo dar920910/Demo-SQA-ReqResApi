2d62715df7276003603bd9561e36b134282cec0f
Denis Rezepov <me@dar920910.ru>
2023-05-11 22:10:43 +0300

    Prepared fake tests for false API versions.

--- src/tests/api/test_suite_api_v2.py
+++ src/tests/api/test_suite_api_v2.py
@@ -1,10 +1,84 @@
-# test_suite_api_v2.py
+""" test_suite_api_v2.py: This module contains fake tests for false REQRES API version 2."""
 
-def test_1():
-    pass
+import pytest
+import datasets.reqres_base_keys as api_keys
+import datasets.reqres_common_data as reqres
+import output
 
-def test_2():
-    pass
 
-def test_3():
-    pass
+# Get the List of Requests' URLs for REQRES API version 2.
+api_v2_urls = reqres.get_sample_requests_urls(reqres.AVAILABLE_API_VERSIONS['v2'])
+
+
+def setup_module(module):
+    print("SETUP: ", module)
+
+def teardown_module(module):
+    print("TEARDOWN: ", module)
+
+
+class TestSuite_APIv2:
+    """ TestSuite_APIv2: This class contains fake tests for false REQRES API version 2. """
+
+    @classmethod
+    def setup_class(cls):
+        print("\nTEST CLASS SETUP:\n{0}: {1}".format(cls.__name__, cls.__doc__))
+
+    @classmethod
+    def teardown_class(cls):
+        print("\nTEST CLASS TEARDOWN: {0}\n".format(cls.__name__))
+
+    def setup_method(self, method):
+        print("\nSetup Test Method: {0}\n".format(method))
+
+    def teardown_method(self, method):
+        print("\nTeardown Test Method: {0}\n".format(method))
+
+    @pytest.mark.parametrize("fake_request_title, fake_request_url, fake_status_code",
+        [
+            (reqres.REQUEST_TITLE_LIST_USERS, api_v2_urls[api_keys.LIST_USERS], reqres.STATUS_CODE_LIST_USERS),
+            (reqres.REQUEST_TITLE_SINGLE_USER, api_v2_urls[api_keys.SINGLE_USER], reqres.STATUS_CODE_SINGLE_USER),
+            (reqres.REQUEST_TITLE_SINGLE_USER_NOT_FOUND, api_v2_urls[api_keys.SINGLE_USER_NOT_FOUND], reqres.STATUS_CODE_SINGLE_USER_NOT_FOUND),
+            (reqres.REQUEST_TITLE_LIST_RESOURCE, api_v2_urls[api_keys.LIST_RESOURCE], reqres.STATUS_CODE_LIST_RESOURCE),
+            (reqres.REQUEST_TITLE_SINGLE_RESOURCE, api_v2_urls[api_keys.SINGLE_RESOURCE], reqres.STATUS_CODE_SINGLE_RESOURCE),
+            (reqres.REQUEST_TITLE_SINGLE_RESOURCE_NOT_FOUND, api_v2_urls[api_keys.SINGLE_RESOURCE_NOT_FOUND], reqres.STATUS_CODE_SINGLE_RESOURCE_NOT_FOUND),
+            (reqres.REQUEST_TITLE_CREATE, api_v2_urls[api_keys.CREATE], reqres.STATUS_CODE_CREATE),
+            (reqres.REQUEST_TITLE_UPDATE_PUT, api_v2_urls[api_keys.UPDATE_PUT], reqres.STATUS_CODE_UPDATE_PUT),
+            (reqres.REQUEST_TITLE_UPDATE_PATCH, api_v2_urls[api_keys.UPDATE_PATCH], reqres.STATUS_CODE_UPDATE_PATCH),
+            (reqres.REQUEST_TITLE_DELETE, api_v2_urls[api_keys.DELETE], reqres.STATUS_CODE_DELETE),
+            (reqres.REQUEST_TITLE_REGISTER_SUCCESSFUL, api_v2_urls[api_keys.REGISTER_SUCCESSFUL], reqres.STATUS_CODE_REGISTER_SUCCESSFUL),
+            (reqres.REQUEST_TITLE_REGISTER_UNSUCCESSFUL, api_v2_urls[api_keys.REGISTER_UNSUCCESSFUL], reqres.STATUS_CODE_REGISTER_UNSUCCESSFUL),
+            (reqres.REQUEST_TITLE_LOGIN_SUCCESSFUL, api_v2_urls[api_keys.LOGIN_SUCCESSFUL], reqres.STATUS_CODE_LOGIN_SUCCESSFUL),
+            (reqres.REQUEST_TITLE_LOGIN_UNSUCCESSFUL, api_v2_urls[api_keys.LOGIN_UNSUCCESSFUL], reqres.STATUS_CODE_LOGIN_UNSUCCESSFUL),
+            (reqres.REQUEST_TITLE_DELAYED_RESPONSE, api_v2_urls[api_keys.DELAYED_RESPONSE], reqres.STATUS_CODE_DELAYED_RESPONSE)
+        ]
+    )
+    def test_response_status_code(self , fake_request_title, fake_request_url, fake_status_code):
+        """test_response_status_code"""
+        output.print_fake_testcase_details(__doc__, fake_request_title, fake_request_url, fake_status_code)
+        pass
+
+
+    @pytest.mark.parametrize("fake_request_title, fake_request_url, fake_response_body",
+        [
+            (reqres.REQUEST_TITLE_LIST_USERS, api_v2_urls[api_keys.LIST_USERS], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_SINGLE_USER, api_v2_urls[api_keys.SINGLE_USER], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_SINGLE_USER_NOT_FOUND, api_v2_urls[api_keys.SINGLE_USER_NOT_FOUND], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_LIST_RESOURCE, api_v2_urls[api_keys.LIST_RESOURCE], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_SINGLE_RESOURCE, api_v2_urls[api_keys.SINGLE_RESOURCE], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_SINGLE_RESOURCE_NOT_FOUND, api_v2_urls[api_keys.SINGLE_RESOURCE_NOT_FOUND], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_CREATE, api_v2_urls[api_keys.CREATE], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_UPDATE_PUT, api_v2_urls[api_keys.UPDATE_PUT], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_UPDATE_PATCH, api_v2_urls[api_keys.UPDATE_PATCH], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_DELETE, api_v2_urls[api_keys.DELETE], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_REGISTER_SUCCESSFUL, api_v2_urls[api_keys.REGISTER_SUCCESSFUL], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_REGISTER_UNSUCCESSFUL, api_v2_urls[api_keys.REGISTER_UNSUCCESSFUL], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_LOGIN_SUCCESSFUL, api_v2_urls[api_keys.LOGIN_SUCCESSFUL], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_LOGIN_UNSUCCESSFUL, api_v2_urls[api_keys.LOGIN_UNSUCCESSFUL], reqres.FAKE_RESPONSE_BODY_V2),
+            (reqres.REQUEST_TITLE_DELAYED_RESPONSE, api_v2_urls[api_keys.DELAYED_RESPONSE], reqres.FAKE_RESPONSE_BODY_V2)
+        ]
+    )
+    def test_response_body_object(self, fake_request_title, fake_request_url, fake_response_body):
+        """test_response_body_object"""
+        output.print_fake_testcase_details(__doc__, fake_request_title, fake_request_url, fake_response_body)
+        pass
