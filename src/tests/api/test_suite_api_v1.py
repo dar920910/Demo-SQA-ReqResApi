165dbf67f5d122bc4aced388097f45d447928a91
Denis Rezepov <me@dar920910.ru>
2023-05-11 15:45:18 +0300

    API tests are moved to TestSuite_APIv1 class.

--- src/tests/api/test_suite_api_v1.py
+++ src/tests/api/test_suite_api_v1.py
@@ -101,89 +101,105 @@ api_tests_response_bodies[api_keys.DELAYED_RESPONSE] = api_response_15.json()
 def print_testing_results(actual_result, expected_result):
     print("\nActual Result: {0}\nExpected Result: {1}\n".format(actual_result, expected_result))
 
-@pytest.mark.parametrize("actual_status_code, expected_status_code",
-    [
-        (api_tests_response_codes[api_keys.LIST_USERS], reqres.STATUS_CODE_LIST_USERS),
-        (api_tests_response_codes[api_keys.SINGLE_USER], reqres.STATUS_CODE_SINGLE_USER),
-        (api_tests_response_codes[api_keys.SINGLE_USER_NOT_FOUND], reqres.STATUS_CODE_SINGLE_USER_NOT_FOUND),
-        (api_tests_response_codes[api_keys.LIST_RESOURCE], reqres.STATUS_CODE_LIST_RESOURCE),
-        (api_tests_response_codes[api_keys.SINGLE_RESOURCE], reqres.STATUS_CODE_SINGLE_RESOURCE),
-        (api_tests_response_codes[api_keys.SINGLE_RESOURCE_NOT_FOUND], reqres.STATUS_CODE_SINGLE_RESOURCE_NOT_FOUND),
-        (api_tests_response_codes[api_keys.CREATE], reqres.STATUS_CODE_CREATE),
-        (api_tests_response_codes[api_keys.UPDATE_PUT], reqres.STATUS_CODE_UPDATE_PUT),
-        (api_tests_response_codes[api_keys.UPDATE_PATCH], reqres.STATUS_CODE_UPDATE_PATCH),
-        (api_tests_response_codes[api_keys.DELETE], reqres.STATUS_CODE_DELETE),
-        (api_tests_response_codes[api_keys.REGISTER_SUCCESSFUL], reqres.STATUS_CODE_REGISTER_SUCCESSFUL),
-        (api_tests_response_codes[api_keys.REGISTER_UNSUCCESSFUL], reqres.STATUS_CODE_REGISTER_UNSUCCESSFUL),
-        (api_tests_response_codes[api_keys.LOGIN_SUCCESSFUL], reqres.STATUS_CODE_LOGIN_SUCCESSFUL),
-        (api_tests_response_codes[api_keys.LOGIN_UNSUCCESSFUL], reqres.STATUS_CODE_LOGIN_UNSUCCESSFUL),
-        (api_tests_response_codes[api_keys.DELAYED_RESPONSE], reqres.STATUS_CODE_DELAYED_RESPONSE)
-    ]
-)
-def test_response_status_code(actual_status_code, expected_status_code):
-    """test_response_status_code"""
-    print_testing_results(actual_status_code, expected_status_code)
-    assert actual_status_code == expected_status_code
-
-
-@pytest.mark.parametrize("actual_response_body, expected_response_body",
-    [
-        (api_tests_response_bodies[api_keys.LIST_USERS], api_json.JSON_BODY_RESPONSE_LIST_USERS),
-        (api_tests_response_bodies[api_keys.SINGLE_USER], api_json.JSON_BODY_RESPONSE_SINGLE_USER),
-        (api_tests_response_bodies[api_keys.SINGLE_USER_NOT_FOUND], api_json.JSON_BODY_RESPONSE_SINGLE_USER_NOT_FOUND),
-        (api_tests_response_bodies[api_keys.LIST_RESOURCE], api_json.JSON_BODY_RESPONSE_LIST_RESOURCE),
-        (api_tests_response_bodies[api_keys.SINGLE_RESOURCE], api_json.JSON_BODY_RESPONSE_SINGLE_RESOURCE),
-        (api_tests_response_bodies[api_keys.SINGLE_RESOURCE_NOT_FOUND], api_json.JSON_BODY_RESPONSE_SINGLE_RESOURCE_NOT_FOUND),
-        (api_tests_response_bodies[api_keys.REGISTER_SUCCESSFUL], api_json.JSON_BODY_RESPONSE_REGISTER_SUCCESSFUL),
-        (api_tests_response_bodies[api_keys.REGISTER_UNSUCCESSFUL], api_json.JSON_BODY_RESPONSE_REGISTER_UNSUCCESSFUL),
-        (api_tests_response_bodies[api_keys.LOGIN_SUCCESSFUL], api_json.JSON_BODY_RESPONSE_LOGIN_SUCCESSFUL),
-        (api_tests_response_bodies[api_keys.LOGIN_UNSUCCESSFUL], api_json.JSON_BODY_RESPONSE_LOGIN_UNSUCCESSFUL),
-        (api_tests_response_bodies[api_keys.DELAYED_RESPONSE], api_json.JSON_BODY_RESPONSE_DELAYED)
-    ]
-)
-def test_response_body_object(actual_response_body, expected_response_body):
-    """test_response_body_object"""
-    print_testing_results(actual_response_body, expected_response_body)
-    assert actual_response_body == expected_response_body
-
-
-
 
 actual_response_07_body = api_tests_response_bodies[api_keys.CREATE]
 actual_response_08_body = api_tests_response_bodies[api_keys.UPDATE_PUT]
 actual_response_09_body = api_tests_response_bodies[api_keys.UPDATE_PATCH]
 
 
-@pytest.mark.parametrize("actual_attr_value, expected_attr_value",
-    [
-        (actual_response_07_body[api_json.ATTR_USER_NAME], api_request_07_body[api_json.ATTR_USER_NAME]),
-        (actual_response_07_body[api_json.ATTR_USER_JOB], api_request_07_body[api_json.ATTR_USER_JOB]),
+class TestSuite_APIv1:
 
-        (actual_response_08_body[api_json.ATTR_USER_NAME], api_request_08_body[api_json.ATTR_USER_NAME]),
-        (actual_response_08_body[api_json.ATTR_USER_JOB], api_request_08_body[api_json.ATTR_USER_JOB]),
+    @classmethod
+    def setup_class(cls):
+        print("\nTestSuite_APIv1: setup_class()...")
+
+    @classmethod
+    def teardown_class(cls):
+        print("\nTestSuite_APIv1: teardown_class()...")
+
+    def setup_method(self, method):
+        print("\nIn setup_method()...")
+
+    def teardown_method(self, method):
+        print("\nIn teardown_method()...")
+
+
+    @pytest.mark.parametrize("actual_status_code, expected_status_code",
+        [
+            (api_tests_response_codes[api_keys.LIST_USERS], reqres.STATUS_CODE_LIST_USERS),
+            (api_tests_response_codes[api_keys.SINGLE_USER], reqres.STATUS_CODE_SINGLE_USER),
+            (api_tests_response_codes[api_keys.SINGLE_USER_NOT_FOUND], reqres.STATUS_CODE_SINGLE_USER_NOT_FOUND),
+            (api_tests_response_codes[api_keys.LIST_RESOURCE], reqres.STATUS_CODE_LIST_RESOURCE),
+            (api_tests_response_codes[api_keys.SINGLE_RESOURCE], reqres.STATUS_CODE_SINGLE_RESOURCE),
+            (api_tests_response_codes[api_keys.SINGLE_RESOURCE_NOT_FOUND], reqres.STATUS_CODE_SINGLE_RESOURCE_NOT_FOUND),
+            (api_tests_response_codes[api_keys.CREATE], reqres.STATUS_CODE_CREATE),
+            (api_tests_response_codes[api_keys.UPDATE_PUT], reqres.STATUS_CODE_UPDATE_PUT),
+            (api_tests_response_codes[api_keys.UPDATE_PATCH], reqres.STATUS_CODE_UPDATE_PATCH),
+            (api_tests_response_codes[api_keys.DELETE], reqres.STATUS_CODE_DELETE),
+            (api_tests_response_codes[api_keys.REGISTER_SUCCESSFUL], reqres.STATUS_CODE_REGISTER_SUCCESSFUL),
+            (api_tests_response_codes[api_keys.REGISTER_UNSUCCESSFUL], reqres.STATUS_CODE_REGISTER_UNSUCCESSFUL),
+            (api_tests_response_codes[api_keys.LOGIN_SUCCESSFUL], reqres.STATUS_CODE_LOGIN_SUCCESSFUL),
+            (api_tests_response_codes[api_keys.LOGIN_UNSUCCESSFUL], reqres.STATUS_CODE_LOGIN_UNSUCCESSFUL),
+            (api_tests_response_codes[api_keys.DELAYED_RESPONSE], reqres.STATUS_CODE_DELAYED_RESPONSE)
+        ]
+    )
+    def test_response_status_code(self, actual_status_code, expected_status_code):
+        """test_response_status_code"""
+        print_testing_results(actual_status_code, expected_status_code)
+        assert actual_status_code == expected_status_code
+
+
+    @pytest.mark.parametrize("actual_response_body, expected_response_body",
+        [
+            (api_tests_response_bodies[api_keys.LIST_USERS], api_json.JSON_BODY_RESPONSE_LIST_USERS),
+            (api_tests_response_bodies[api_keys.SINGLE_USER], api_json.JSON_BODY_RESPONSE_SINGLE_USER),
+            (api_tests_response_bodies[api_keys.SINGLE_USER_NOT_FOUND], api_json.JSON_BODY_RESPONSE_SINGLE_USER_NOT_FOUND),
+            (api_tests_response_bodies[api_keys.LIST_RESOURCE], api_json.JSON_BODY_RESPONSE_LIST_RESOURCE),
+            (api_tests_response_bodies[api_keys.SINGLE_RESOURCE], api_json.JSON_BODY_RESPONSE_SINGLE_RESOURCE),
+            (api_tests_response_bodies[api_keys.SINGLE_RESOURCE_NOT_FOUND], api_json.JSON_BODY_RESPONSE_SINGLE_RESOURCE_NOT_FOUND),
+            (api_tests_response_bodies[api_keys.REGISTER_SUCCESSFUL], api_json.JSON_BODY_RESPONSE_REGISTER_SUCCESSFUL),
+            (api_tests_response_bodies[api_keys.REGISTER_UNSUCCESSFUL], api_json.JSON_BODY_RESPONSE_REGISTER_UNSUCCESSFUL),
+            (api_tests_response_bodies[api_keys.LOGIN_SUCCESSFUL], api_json.JSON_BODY_RESPONSE_LOGIN_SUCCESSFUL),
+            (api_tests_response_bodies[api_keys.LOGIN_UNSUCCESSFUL], api_json.JSON_BODY_RESPONSE_LOGIN_UNSUCCESSFUL),
+            (api_tests_response_bodies[api_keys.DELAYED_RESPONSE], api_json.JSON_BODY_RESPONSE_DELAYED)
+        ]
+    )
+    def test_response_body_object(self, actual_response_body, expected_response_body):
+        """test_response_body_object"""
+        print_testing_results(actual_response_body, expected_response_body)
+        assert actual_response_body == expected_response_body
+
+
+    @pytest.mark.parametrize("actual_attr_value, expected_attr_value",
+        [
+            (actual_response_07_body[api_json.ATTR_USER_NAME], api_request_07_body[api_json.ATTR_USER_NAME]),
+            (actual_response_07_body[api_json.ATTR_USER_JOB], api_request_07_body[api_json.ATTR_USER_JOB]),
+
+            (actual_response_08_body[api_json.ATTR_USER_NAME], api_request_08_body[api_json.ATTR_USER_NAME]),
+            (actual_response_08_body[api_json.ATTR_USER_JOB], api_request_08_body[api_json.ATTR_USER_JOB]),
         
-        (actual_response_09_body[api_json.ATTR_USER_NAME], api_request_09_body[api_json.ATTR_USER_NAME]),
-        (actual_response_09_body[api_json.ATTR_USER_JOB], api_request_09_body[api_json.ATTR_USER_JOB])
-    ]
-)
-def test_user_object_attribute_is_equal(actual_attr_value, expected_attr_value):
-    """test_user_object_attribute_is_equal"""
-    print_testing_results(actual_attr_value, expected_attr_value)
-    assert actual_attr_value == expected_attr_value
+            (actual_response_09_body[api_json.ATTR_USER_NAME], api_request_09_body[api_json.ATTR_USER_NAME]),
+            (actual_response_09_body[api_json.ATTR_USER_JOB], api_request_09_body[api_json.ATTR_USER_JOB])
+        ]
+    )
+    def test_user_object_attribute_is_equal(self, actual_attr_value, expected_attr_value):
+        """test_user_object_attribute_is_equal"""
+        print_testing_results(actual_attr_value, expected_attr_value)
+        assert actual_attr_value == expected_attr_value
 
 
-@pytest.mark.parametrize("datetime_attr_value",
-    [
-        actual_response_07_body[api_json.ATTR_USER_CREATED],
-        actual_response_08_body[api_json.ATTR_USER_UPDATED],
-        actual_response_09_body[api_json.ATTR_USER_UPDATED]
-    ]
-)
-def test_datetime_attribute_is_not_empty(datetime_attr_value):
-    """test_datetime_attribute_is_not_empty"""
-    assert datetime_attr_value != None
+    @pytest.mark.parametrize("datetime_attr_value",
+        [
+            actual_response_07_body[api_json.ATTR_USER_CREATED],
+            actual_response_08_body[api_json.ATTR_USER_UPDATED],
+            actual_response_09_body[api_json.ATTR_USER_UPDATED]
+        ]
+    )
+    def test_datetime_attribute_is_not_empty(self, datetime_attr_value):
+        """test_datetime_attribute_is_not_empty"""
+        assert datetime_attr_value != None
 
 
-def test_created_user_id_is_positive_value():
-    """test_created_user_id_is_positive_value"""
-    assert int(actual_response_07_body[api_json.ATTR_USER_ID]) > 0
+    def test_created_user_id_is_positive_value(self):
+        """test_created_user_id_is_positive_value"""
+        assert int(actual_response_07_body[api_json.ATTR_USER_ID]) > 0
